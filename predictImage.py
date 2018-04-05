6f88f4a0f890ba22731b3416879c671b95d04bfa
Ben Miller <benkenji@alumni.ubc.ca>
2018-04-05 02:32:23 -0700

    asdf

--- predictImage.py
+++ predictImage.py
@@ -27,7 +27,7 @@ def getNames():
 
 def getPerson(image):
     cropped_image = getCropped(image)
-    
+
 
     model = cv2.face.LBPHFaceRecognizer_create()
     model.read('trainer.yml')
@@ -38,6 +38,9 @@ def getPerson(image):
 
 def isPerson(imagepath, username):
     image = cv2.imread(imagepath, 0)
+    #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    #image = clahe.apply(im)
+
     face_cascade = cv2.CascadeClassifier(haar_file)
     #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(image, 1.3, 4)
@@ -45,12 +48,61 @@ def isPerson(imagepath, username):
         print("no faces!!!")
         return 0
     vals = getPerson(image)
-    
+
     if vals[0] == username and vals[1] < 97:
         print(vals[0])
         print(str(vals[1]))
         return 1
-    else: 
+    else:
         print(vals[0])
         print(str(vals[1]))
         return 0
+
+
+'''
+import face_recognition
+import cv2
+
+def isPerson(path):
+	img2 = cv2.imread("./1.jpg", 0)
+	cv2.imwrite("./1.jpg", img2)
+	picture_of_me = face_recognition.load_image_file("./1.jpg")
+
+	#face_locations = face_recognition.face_locations(picture_of_me)
+	#print(face_locations)
+
+	encodings = face_recognition.face_encodings(picture_of_me)
+	if len(encodings) > 0:
+		my_face_encoding = encodings[0]
+	else:
+		print("No face detected in the picture! Quitting!")
+		return 0
+
+	img = cv2.imread(path, 0)
+	cv2.imwrite(path, img)
+	unknown = face_recognition.load_image_file(path)
+
+	#face_locations = face_recognition.face_locations(unknown)
+	#print(face_locations)
+
+	#picture_of_me = face_recognition.load_image_file("obama.jpg")
+	#print(picture_of_me)
+	#my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+	encodings2 = face_recognition.face_encodings(unknown)
+	if len(encodings2) > 0:
+		unknown_face_encoding = encodings2[0]
+	else:
+		print("No face detected in the picture! Quitting!")
+		return 0
+
+	results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
+
+	if results[0] == True:
+		print("success")
+		return 1
+	else:
+		print("not me")
+		return 0
+
+#isPerson("./2.jpg")
+'''
